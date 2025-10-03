bf41d0b8d0595cecda34d9a06270b3a3964f3856
Matheus-Ribeiro-Jesus <pessoalsenacsp@gmail.com>
2025-10-03 17:09:14 -0300

    Arrumando as APIS

--- controllers/ValidadorController.php
+++ controllers/ValidadorController.php
@@ -2,14 +2,18 @@
 
 class ValidatorController{
 
-    public static function validate_data($data, $labels){
+     public static function validate_data($data, $labels){
         $pendets = [];
         foreach ($labels as $lbl){
             if (!isset($data[$lbl]) && empty($data[$lbl]) ){
                 $pendets[] = $lbl;
             }
         }
-        return $pendets;
+        if(!empty($pendets) ){
+            $pendets = implode(", ", $pendets);
+            jsonResponse(['message'=> "Erro, Falta o campo: ".$pendets], 400);
+            exit;
+        }
     }
 
     public static function fix_hours($data, $horas){
